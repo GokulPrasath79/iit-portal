f51bb8df25bd073b13e25ba8455a92e786ab3547
Gokul Prasath Venkatesh <gokulprasath.079@gmail.com>
2026-06-26 19:35:50 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,15 +3,11 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDsx74IRIMYOAQ2nxk3sIePlsPLZ4H3K9A",
-  authDomain: "iit-portal.firebaseapp.com",
-  projectId: "iit-portal",
-  storageBucket: "iit-portal.firebasestorage.app",
-  messagingSenderId: "246658745051",
-  appId: "1:246658745051:web:8b5efc39c85f16b2a3c2fe",
-  measurementId: "G-XN6CHQ9EMP"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
-
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
